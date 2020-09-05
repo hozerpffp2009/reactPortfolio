ea2d988017c8387ba5098bbc428ea07ee8b8321a
hozerpffp <hozerpffp2009@gmail.com>
2020-09-04 22:09:03 -0500

    Updates

---
+++ precache-manifest.cb30432ba85c1b97161edef0ca1d629a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3430f12db2f15ca80210ee6c46e72ec6",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "87ff15699b27a2a14923",
+    "url": "/reactPortfolio/static/css/main.2cab2c26.chunk.css"
+  },
+  {
+    "revision": "679fb318a548ba673dd4",
+    "url": "/reactPortfolio/static/js/2.6c2bba00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPortfolio/static/js/2.6c2bba00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ff15699b27a2a14923",
+    "url": "/reactPortfolio/static/js/main.3c418d46.chunk.js"
+  },
+  {
+    "revision": "beded403309fe67800d5",
+    "url": "/reactPortfolio/static/js/runtime-main.8278393b.js"
+  }
+]);
