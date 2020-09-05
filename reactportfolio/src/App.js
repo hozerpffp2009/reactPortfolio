cc6c2bae732f78ca6be2f190e1fb71edf0d47335
hozerpffp <hozerpffp2009@gmail.com>
2020-09-04 21:50:50 -0500

    made styling changes. got resume to work

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
 import About from './components/About';
 import Work from './components/Work';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar'
+
 
 function App() {
   return (
